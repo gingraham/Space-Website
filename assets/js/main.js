4cef9301f59587625da090dffc1c0d3b420e6ebf
Gerald Ingraham <geraldingraham@Geralds-MBP.attlocal.net>
2023-08-16 14:25:31 -0400

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,30 @@
 /*=============== SHOW MENU ===============*/
+const menu = document.querySelector('#nav-menu'),
+      toggle_btn = document.querySelector('#nav-toggle'),
+      close_btn = document.querySelector('#nav-close')
+      navLinks = document.querySelectorAll(".nav__link")
+// Validate if toggle btn exists show when clicked
+if(toggle_btn){
+    toggle_btn.addEventListener('click', ()=>{
+        menu.classList.add("show-menu")
+      })
+}
 
-
-/*=============== REMOVE MENU MOBILE ===============*/
-
+// Hide the menu when close btn clicked
+if(close_btn){
+    close_btn.addEventListener('click', ()=>{
+        menu.classList.remove('show-menu')
+      })
+}
+/*=============== REMOVE MOBILE MENU WHEN NAV ITEM CLICKED ===============*/
+//  define function to add to each link
+const linkAction = () =>{
+    const navMenu = document.querySelector('#nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLinks.forEach(link => {
+    link.addEventListener('click',linkAction )
+});
 
 /*=============== GSAP ANIMATION ===============*/
 
